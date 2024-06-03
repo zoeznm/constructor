5470c19814e469836fde456855c870cce95aba2a
zoeznm <bomikim046@gmail.com>
2024-06-03 11:52:43 +0900

    수업

--- work-1.js
+++ work-1.js
@@ -2,9 +2,18 @@
 // * 목적은 함수를 조금 더 이해하려고 합니다.
 // * 매개변수가 객체인 경우가 아래에 작성될 예정입니다. 
 
-function workOne(a,b) {
-// ! logic: 매개변수 두개를 받은 다음
-// ! logic: return은 객체였음 좋겠다.
+function workOne(a, b) {
 
-return 오브젝트;
+  let localVariable = {
+     a:a,
+     b:b
+  };
+
+   return localVariable;
+
 }
+
+console.log(workOne('a', 'b')); 
+
+
+
