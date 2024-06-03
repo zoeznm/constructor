a51c1a746f570235b920618ddb99a85ecad9a9e9
zoeznm <bomikim046@gmail.com>
2024-06-03 12:46:06 +0900

    test-4

---
+++ test-4.js
@@ -0,0 +1,34 @@
+const names = [
+  "구하림",
+  "김보미",
+  "김수현",
+  "김정수",
+  "문혜림",
+  "배성빈",
+  "백지원",
+  "송이현",
+  "신지윤",
+  "유으뜸",
+  "유호영",
+  "이연승",
+  "이재영",
+  "이종수",
+  "임유진",
+  "정호연",
+  "조우식",
+  "조자연",
+  "최유진",
+  "황재민"
+];
+
+const makeMember = require('./work-3.js');
+console.log(makeMember(1, "김보미"));
+
+let arrayMembers=[];
+
+for (let i=0; i <names.length; i++) {
+  let member = makeMember(i, names[i])
+  arrayMembers.push(member);
+}
+
+console.log(arrayMembers);
